c1a384b5ffb15835142bfa151c19ef95c6d430a3
Kenny Williams <kwill@hey.com>
2024-08-29 08:26:51 -0700

    add vite and next pages router examples

---
+++ apps/next-repro/src/pages/grid-page.tsx
@@ -0,0 +1,6 @@
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+import ExamplePage from '../app/page.tsx';
+
+export default function GridPage() {
+  return <AntdRegistry><ExamplePage/></AntdRegistry>
+}
