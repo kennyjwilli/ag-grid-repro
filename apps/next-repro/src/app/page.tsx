3733f8f145e0050862c178de4cc847dde26474cf
Kenny Williams <kwill@hey.com>
2024-10-05 10:08:06 -0700

    add AsyncRenderer

--- apps/next-repro/src/app/page.tsx
+++ apps/next-repro/src/app/page.tsx
@@ -5,7 +5,8 @@ import '@ag-grid-community/styles/ag-theme-quartz.css';
 
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
 import { ModuleRegistry } from '@ag-grid-community/core';
-import { AgGridReact } from '@ag-grid-community/react';
+import { AgGridReact, type CustomCellRendererProps } from '@ag-grid-community/react';
+import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
 import { LicenseManager } from '@ag-grid-enterprise/core';
 import _ from 'lodash';
 import React from 'react';
@@ -14,10 +15,24 @@ LicenseManager.setLicenseKey(
   'Using_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-060054}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Deep_Origin}_is_granted_a_{Single_Application}_Developer_License_for_the_application_{Deep_Origin}_only_for_{2}_Front-End_JavaScript_developers___All_Front-End_JavaScript_developers_working_on_{Deep_Origin}_need_to_be_licensed___{Deep_Origin}_has_been_granted_a_Deployment_License_Add-on_for_{1}_Production_Environment___This_key_works_with_{AG_Charts_and_AG_Grid}_Enterprise_versions_released_before_{1_March_2025}____[v3]_[0102]_MTc0MDc4NzIwMDAwMA==88258d6b91a0e71396f764713cc90285',
 );
 
-ModuleRegistry.registerModules([ClientSideRowModelModule]);
+ModuleRegistry.registerModules([ClientSideRowModelModule, ColumnsToolPanelModule]);
 
-const CellRenderer = () => {
+const AsyncRenderer = React.memo((props: React.PropsWithChildren) => {
+  const { children } = props;
+  const [first, setFirst] = React.useState(true);
 
+  if (first) {
+    Promise.resolve().then(() => {
+      setFirst(false);
+    });
+  }
+
+  return first ? null : children;
+});
+AsyncRenderer.displayName = 'AsyncRenderer';
+
+const CellRenderer = (props: CustomCellRendererProps) => {
+  const { data } = props;
   const [a, setA] = React.useState<string>();
   const [b, setB] = React.useState<string>();
   React.useEffect(() => {
@@ -25,17 +40,20 @@ const CellRenderer = () => {
     setB('b');
   }, []);
   return (
-    <div>
-      {a}
-      {b}
-    </div>
+    <AsyncRenderer>
+      <div>
+        {data?.index}
+        {a}
+        {b}
+      </div>
+    </AsyncRenderer>
   );
 };
 
 const RootContainer = () => {
   const gridRef = React.useRef(null);
 
-  const rows = _.range(0, 200).map((i) => ({
+  const rows = _.range(0, 600).map((i) => ({
     index: `${i}`,
   }));
 
@@ -51,7 +69,11 @@ const RootContainer = () => {
         ref={gridRef}
         rowData={rows}
         columnDefs={columnDefs}
+        // suppressColumnVirtualisation
+        // suppressRowVirtualisation
+        // suppressMaxRenderedRowRestriction
         getRowId={(params) => params.data.index ?? '--'}
+        sideBar="columns"
       />
     </div>
   );
